806978881e99f8ed17f7acff9e05ae8d34892c50
EvgenSakun <evgen.sakun20@gmail.com>
2024-02-13 19:48:45 +0300

    start

---
+++ HM22/HM22/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HM22
+//
+//  Created by Evgeny Sakun on 13.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
